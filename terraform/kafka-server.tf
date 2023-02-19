d42872bbe2eed36ce3b03a7aebd53fab27a243fb
Duy Mai <duymai1612@gmail.com>
2023-02-19 10:16:29 +0700

    update droplet name

--- terraform/kafka-server.tf
+++ terraform/kafka-server.tf
@@ -1,7 +1,7 @@
 # Create a new Web Droplet in the nyc2 region
-resource "digitalocean_droplet" "kafka_server" {
+resource "digitalocean_droplet" "kafka" {
   image  = "ubuntu-20-04-x64"
-  name   = "kafka_server"
+  name   = "kafka"
   region = "sgp1"
   size   = "s-4vcpu-8gb"
   ssh_keys = [var.ssh_key_id]
