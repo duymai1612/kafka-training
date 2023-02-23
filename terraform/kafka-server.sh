cc5dd1fb7ab43367249c1c12acf99c91ac8f1aea
Duy Mai <duymai1612@gmail.com>
2023-02-23 09:08:38 +0700

    add cd command

--- terraform/kafka-server.sh
+++ terraform/kafka-server.sh
@@ -8,26 +8,9 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# INSTALL GIT
-# sudo apt update
-# sudo apt install libz-dev libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext cmake gcc
-# mkdir tmp
-# cd /tmp
-# curl -o git.tar.gz https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.26.2.tar.gz
-# tar -zxf git.tar.gz
-# cd git-*
-# make prefix=/usr/local all
-# sudo make prefix=/usr/local install
-# exec bash
-# git --version
-# git config --global user.name "Your Name"
-# git config --global user.email "youremail@domain.com"
-# git config --list
-# nano ~/.gitconfig
-
-
 # INSTALL KAFKA, ZOOKEEPER, KAFKA-UI
 mkdir -p ~/kafka
+cd ~/kafka
 
 echo "
 version: '3'
