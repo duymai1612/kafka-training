0922dfd2e468f5f353e9191f67bc55f57deb283e
Duy Mai <duymai1612@gmail.com>
2023-02-23 09:34:52 +0700

    add output kafka ip

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,8 @@
 # output "droplet_output" {
 #   value = data.digitalocean_droplet.kafka.ipv4_address
 # }
+
+output "ip_output" {
+#   value = data.digitalocean_reserved_ip.example.droplet_id
+  value = digitalocean_reserved_ip.kafka.ip_address
+}
