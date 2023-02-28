d85fcddecd6b8026f6d4bf01314fbb2fb1c80f16
Duy Mai <duymai1612@gmail.com>
2023-02-28 09:20:04 +0700

    add nginx proxy to shell script

--- terraform/kafka-server.sh
+++ terraform/kafka-server.sh
@@ -62,6 +62,19 @@ services:
       - zookeeper
     networks:
       - kafka-network
+
+  proxy:
+    image: 'jc21/nginx-proxy-manager:latest'
+    restart: unless-stopped
+    ports:
+      - '80:80'
+      - '81:81'
+      - '443:443'
+    volumes:
+      - ./data:/data
+      - ./letsencrypt:/etc/letsencrypt
+    networks:
+      - kafka-network
   
 networks:
   kafka-network:
